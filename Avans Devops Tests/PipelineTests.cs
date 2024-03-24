bfe0506fcd2e4ff58aceda32bb995a31ba6f1b63
Jelmer Verhoeven <jjp.verhoeven@student.avans.nl>
2024-03-24 20:19:54 +0100

    *bullshit*

--- Avans Devops Tests/PipelineTests.cs
+++ Avans Devops Tests/PipelineTests.cs
@@ -174,5 +174,74 @@ namespace Avans_Devops_Tests
             pipelineVisitor.Verify(x => x.Visit(It.IsAny<Package>()), Times.AtLeastOnce);
             pipelineVisitor.Verify(x => x.Visit(It.IsAny<Pipeline>()), Times.AtLeastOnce);
         }
+
+        [Fact]
+        public void can_run_full_pipelinevisitor()
+        {
+            //Arrange
+            var pipelineVisitor = new PipelineVisitor();
+            var pipeline = new Pipeline("Pipeline");
+
+            //Act
+
+            //Source
+            var source = new SourceContainer("SourceContainer");
+            var azureSource = new SourceAzure("SourceAzure");
+            var githubSource = new SourceGithub("githubSource");
+            source.AddComponent(azureSource);
+            pipeline.AddComponent(source);
+            source.AddComponent(githubSource);
+            //Package
+            var package = new Package("Package");
+            pipeline.AddComponent(package);
+            //Build
+            var build = new BuildContainer("BuildContainer");
+            var mavenBuild = new BuildMaven("BuildMaven");
+            var jenkinsBuild = new BuildJenkins("BuildJenkins");
+            var buildAnt = new BuildAnt("ant");
+            build.AddComponent(jenkinsBuild);
+            build.AddComponent(mavenBuild);
+            build.AddComponent(buildAnt);
+            pipeline.AddComponent(build);
+            //Testing
+            var testContainer = new TestContainer("TestContainer");
+            var seleniumTest = new SeleniumTests("SeleniumTests");
+            var nUnitTest = new NUnitTests("Nunit");
+            testContainer.AddComponent(seleniumTest);
+            testContainer.AddComponent(nUnitTest);
+            pipeline.AddComponent(testContainer);
+            //Analyse
+            var analyseContainer = new AnalyseContainer("analyseContainer");
+            var sonarQube = new AnalyseSonarQube("SonarQube");
+            var sonarQubeExe = new SonarQubeExecute("SonarQube exe");
+            var sonarQubeReport = new SonarQubeReporting("SonarQubeReport");
+            analyseContainer.AddComponent(sonarQube);
+            sonarQube.AddComponent(sonarQubeExe);
+            sonarQube.AddComponent(sonarQubeReport);
+            pipeline.AddComponent(analyseContainer);
+
+            //Deploy
+            var deployContainer = new DeployContainer("deploy");
+            var deployAzure = new DeployAzure("azure");
+            var deployAWS = new DeployAWS("aws");
+            pipeline.AddComponent(deployContainer);
+            deployContainer.AddComponent(deployAzure);
+            deployContainer.AddComponent(deployAWS);
+
+            // Utility
+            var utility = new Utility("Utility");
+            var commando = new Action(() => Console.WriteLine("Doing something after"));
+            var commando2 = new Action(() => Console.WriteLine("Doing something after 2"));
+            var commando3 = new Action(() => Console.WriteLine("Doing something after 3"));
+            utility.AddAction(commando);
+            utility.AddAction(commando2);
+            utility.AddAction(commando3);
+            pipeline.AddComponent(utility);
+
+            //Assert
+            pipeline.AcceptVisitor(pipelineVisitor);
+
+            Assert.True(true);
+        }
     }
 }
